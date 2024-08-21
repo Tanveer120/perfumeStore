53cb890ade3bee789d8355fb5795c028d0965474
Tanveer120 <tanveershaikh.3101@gmail.com>
2024-08-21 10:28:29 -0700

    homepage product card

--- home.php
+++ home.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="home.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    </style>
 </head>
+
 <body>
-<header>
+    <header>
         <div class="nav_container">
-            <img src = "./assets/images/logo.png" class="navLogo">
-            
+            <img src="./assets/images/logo.png" class="navLogo">
+
             <div class="searchCont">
                 <div class="searchBar">
                     <i class="fa-solid fa-magnifying-glass" id="searchIcon"></i>
@@ -59,10 +64,76 @@
         <div class="productCard">
             <img src="./assets/images/prod1.png" alt="prod1" class="prodImg">
             <h1 class="prodName">Form Refume</h1>
-            <p class="price"></p>
+            <p class="price"><b>Price: </b>2000</p>
+            <button class="btn"> Buy
+            </button>
+        </div>
+        <div class="productCard">
+            <img src="./assets/images/prod1.png" alt="prod1" class="prodImg">
+            <h1 class="prodName">Form Refume</h1>
+            <p class="price"><b>Price: </b>2000</p>
+            <button class="btn">Buy
+            </button>
+        </div>
+        <div class="productCard">
+            <img src="./assets/images/prod1.png" alt="prod1" class="prodImg">
+            <h1 class="prodName">Form Refume</h1>
+            <p class="price"><b>Price: </b>2000</p>
+            <button class="btn"> Buy
+            </button>
+        </div>
+        <div class="productCard">
+            <img src="./assets/images/prod1.png" alt="prod1" class="prodImg">
+            <h1 class="prodName">Form Refume</h1>
+            <p class="price"><b>Price: </b>2000</p>
+            <button class="btn"> Buy
+            </button>
+        </div>
+        <div class="productCard">
+            <img src="./assets/images/prod1.png" alt="prod1" class="prodImg">
+            <h1 class="prodName">Form Refume</h1>
+            <p class="price"><b>Price: </b>2000</p>
+            <button class="btn"> Buy
+            </button>
+        </div>
+        <div class="productCard">
+            <img src="./assets/images/prod1.png" alt="prod1" class="prodImg">
+            <h1 class="prodName">Form Refume</h1>
+            <p class="price"><b>Price: </b>2000</p>
+            <button class="btn"> Buy
+            </button>
+        </div>
+        <div class="productCard">
+            <img src="./assets/images/prod1.png" alt="prod1" class="prodImg">
+            <h1 class="prodName">Form Refume</h1>
+            <p class="price"><b>Price: </b>2000</p>
+            <button class="btn">Buy
+            </button>
+        </div>
+        <div class="productCard">
+            <img src="./assets/images/prod1.png" alt="prod1" class="prodImg">
+            <h1 class="prodName">Form Refume</h1>
+            <p class="price"><b>Price: </b>2000</p>
+            <button class="btn"> Buy
+            </button>
+        </div>
+        <div class="productCard">
+            <img src="./assets/images/prod1.png" alt="prod1" class="prodImg">
+            <h1 class="prodName">Form Refume</h1>
+            <p class="price"><b>Price: </b>2000</p>
+            <button class="btn"> Buy
+            </button>
+        </div>
+        <div class="productCard">
+            <img src="./assets/images/prod1.png" alt="prod1" class="prodImg">
+            <h1 class="prodName">Form Refume</h1>
+            <p class="price"><b>Price: </b>2000</p>
+            <button class="btn"> Buy
+            </button>
         </div>
     </div>
-    
+
     <script src="nav.js"></script>
 </body>
+
 </html>
